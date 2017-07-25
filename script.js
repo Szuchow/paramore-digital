3696bcbbda9ec6b88862859f817fc64516c18dc2
Szuchow <klawa97@interia.pl>
2017-07-25 16:32:11 +0200

    Problem

---
+++ script.js
@@ -0,0 +1,13 @@
+window.addEventListener('DOMContentLoaded', function () {
+	var btn = document.getElementById('button');
+	var show = document.getElementById('show');
+	var hide = document.getElementById('hide');
+	var menu = document.getElementById('responsive-menu');
+
+	btn.addEventListener('click', function () {
+		menu.classList.toggle('disabled');
+		hide.classList.toggle('disabled');
+		show.classList.toggle('disabled');
+	});
+
+});
